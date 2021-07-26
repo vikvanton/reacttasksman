51d029ab62d3b0e458595f34bb89ec732f5b3f21
Gaiseric <gaisericvandalorum@gmail.com>
2021-07-26 17:16:48 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import StorageManager from "../components/StorageManager"; 
 import TaskList from "../components/TaskList";
 import AddTaskForm from "../components/AddTaskForm";
